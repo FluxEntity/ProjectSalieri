5c483c600e799312a01caa5c7457e67c1dc04c18
Maxwell Archibald <maxwellmarchibald@utexas.edu>
2017-06-14 21:38:06 -0500

    Memory Serialization & Dialogue
    Serialization: Classes are now able to be added to PlayerPrefs through
    serializing them, furthering the capability of the save/load system
    Dialogue: NPCs can hold text file references and pass them to the
    dialogue manager when the player talks to the NPC.

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameManager gameManager;
+
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endLine;
+
+
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+        dialogueBox.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameManager.InDialogue)
+        {
+            dialogueText.text = textLines[currentLine];
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine++;
+        }
+        if (currentLine > endLine)
+        {
+            dialogueBox.SetActive(false);
+            gameManager.InDialogue = false;
+        }
+    }
+
+    public void AcceptText()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        currentLine = 0;
+        endLine = textLines.Length - 1;
+    }
+
+    public void DisplayTextBox()
+    {
+        gameManager.InDialogue = true;
+        dialogueBox.SetActive(true);
+    }
+}
--- Assets/Scripts/Stats/Classes/BaseEngineerClass.cs
+++ Assets/Scripts/Stats/Classes/BaseEngineerClass.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
 public class BaseEngineerClass : BaseCharacterClass {
 
 	public BaseEngineerClass()
