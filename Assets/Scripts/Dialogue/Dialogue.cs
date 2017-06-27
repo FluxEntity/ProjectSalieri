74c562d3629750f937848aeda44300d04b09fbb5
Maxwell Archibald <maxwellmarchibald@utexas.edu>
2017-06-27 15:27:28 -0500

    Dialogue System Update
    Added more functions to dialogue system, with conversations that can be
    held in objects like NPCs and the ability to create player choice.

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour {
+public class Dialogue {
 
     public List<DialogueNode> dialogueTree;
 
--- Assets/Scripts/Dialogue/TestDialogue.cs
+++ Assets/Scripts/Dialogue/TestDialogue.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestDialogue : Dialogue {
+public class TestDialogue : MonoBehaviour {
 
-    //Dialogue tester = new Dialogue();
+    Dialogue tester = new Dialogue();
 
 	// Use this for initialization
 	void Start () {
         InitializeDialogue();
-        gameObject.GetComponent<DialogueHolder>().experiment = this;
+        GetComponent<DialogueHolder>().experiment = tester;
 	}
 	
 	// Update is called once per frame
@@ -17,19 +17,19 @@ public class TestDialogue : Dialogue {
 		
 	}
 
-    new void InitializeDialogue()
+    void InitializeDialogue()
     {
         DialogueNode node1 = new DialogueNode("This a presentation of Project Salieri's dialogue tree.", "This means that the string arrays work.");
         DialogueNode node2 = new DialogueNode("I hope you will find this game to your liking!");
         DialogueNode node3 = new DialogueNode("Alright, now let's test this test!");
         DialogueNode node4 = new DialogueNode("Who put you on the planet?");
 
-        this.AddNode(node1);
-        this.AddNode(node2);
-        this.AddNode(node3);
-        this.AddNode(node4);
+        tester.AddNode(node1);
+        tester.AddNode(node2);
+        tester.AddNode(node3);
+        tester.AddNode(node4);
 
-        this.AddOption("Yes", node1, node3);
-        this.AddOption("No", node1, node4);
+        tester.AddOption("Yes", node1, node3);
+        tester.AddOption("No", node1, node4);
     }
 }
